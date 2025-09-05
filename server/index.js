5f389511d2deb79875633307d13655f674272cbf
Sarah Fajriah Rahmah <sarahfajriarahmah@gmail.com>
2025-09-05 12:42:56 +0700

    change import cors

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 const app = express();
 const port = process.env.PORT || 5000;
 
-import cors from 'cors';
-
 const corsOptions = {
   origin: [
     'https://rimuru.up.railway.app',
